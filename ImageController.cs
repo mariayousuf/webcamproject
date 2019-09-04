31e7c7f6ea9d2a31dbc1fa357096b625ba222565
mariayousuf <mariayousuf96@gmail.com>
2019-09-04 13:58:02 +0500

    Add files via upload

---
+++ ImageController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using image_processing.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace image_processing.Controllers
+{
+    public class ImageController : Controller
+    {
+        // GET: Image
+        public ActionResult Capture()
+        {
+
+            return View();
+
+        }
+        [HttpPost]
+        public JsonResult capture_image(string name)
+        {
+            PhotoCaptureEntities db = new PhotoCaptureEntities();
+            Image imageStore = new Image()
+            {
+                CreateDate = DateTime.Now,
+                ImageBase64String = name,
+                ImageId = 0
+
+
+             
+        };
+            db.Images.Add(imageStore);
+
+
+            db.SaveChanges();
+
+
+            return Json(true);
+        }
+           
+    }
+}
+
+
+
+     
